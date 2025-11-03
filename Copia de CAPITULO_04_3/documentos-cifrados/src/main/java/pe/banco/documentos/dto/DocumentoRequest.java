805fa1b0b15b439fb73d2b8d56fa8827d096b44b
RodrigoMSB <101293425+RodrigoMSB@users.noreply.github.com>
2025-11-03 12:47:19 -0300

    Nuevas versiones

--- Copia de CAPITULO_04_3/documentos-cifrados/src/main/java/pe/banco/documentos/dto/DocumentoRequest.java
+++
@@ -1,9 +0,0 @@
-package pe.banco.documentos.dto;
-
-public class DocumentoRequest {
-    public String titulo;
-    public String contenido;  // Este llegará en texto plano y se cifrará
-
-    public DocumentoRequest() {
-    }
-}
